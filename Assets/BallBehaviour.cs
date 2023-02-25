25c4d9c3f4e7ad0979014da7bc9805aeaa44bf81
cozz-py <bencozzens13@gmail.com>
2023-02-25 08:37:34 -0500

    Commit with comments

--- Assets/BallBehaviour.cs
+++ Assets/BallBehaviour.cs
@@ -13,6 +13,8 @@ public class BallBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Vector3 = y axis, 180 degrees
+        // Time.deltaTime turns it by seconds instead of frames, this is static
+        transform.Rotate(Vector3.up, 180 * Time.deltaTime);
     }
 }
