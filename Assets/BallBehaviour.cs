67b8421d2b36db87eb6d5d584141b77e20b99d1a
chapcs <benchapman003@gmail.com>
2023-03-01 08:32:04 -0500

    Added debug ray for vector reference

--- Assets/BallBehaviour.cs
+++ Assets/BallBehaviour.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,8 @@ public class BallBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, 180 * Time.deltaTime);
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.Rotate(axis, DegreesPerSecond * Time.deltaTime);
+        Debug.DrawRay(Vector3.zero, axis, Color.yellow, .5f);
     }
 }
